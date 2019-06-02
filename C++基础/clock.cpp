d110c40120f4c4d0b24ef3341a5ea6bd52683b6a
JoshuaQSH <1249646094@qq.com>
2019-06-03 00:10:49 +0800

    clock.cpp changed

--- C++基础/clock.cpp
+++ C++基础/clock.cpp
@@ -11,9 +11,9 @@ void usingclock()
 	HANDLE hStdout;    
     COORD cursorPos;   
 	int hour=0,min=0,sec=0;
-	while(hour<1){
+	while(hour<99){
 		hStdout = GetStdHandle(STD_OUTPUT_HANDLE);  
-		Sleep(1);
+		Sleep(1000);
 		sec++;
 		if(sec == 60){
 			sec = 0;
@@ -34,38 +34,8 @@ void usingclock()
 	CloseHandle(hStdout);
 }
 
-int partsort(int* array, int left, int right)
-{
-	int key = array[left];
-	while(left<right){
-		while(left<right && array[left]<=key){
-			left++;
-		}
-		array[right] = array[left];
-		while(left<right && array[right]>=key){
-			right++;
-		}
-		array[left] = array[right];
-	}
-	array[right] = key;
-	return right;
-}
-
-void quicksort(int* array, int left, int right)
-{
-	if(left>=right)
-		return;
-	int index = partsort(array,left,right);
-	quicksort(array,left,index-1);
-	quicksort(array,index+1,right);
-}
-	
-
 int main()
 {
-//	usingclock();
-	int *array;
-	array = [1,5,6,2];
-	quicksort(array,0,array.size()-1);
+	usingclock();
 	return 0;	
 }
