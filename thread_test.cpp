679a1a562a708dbea3f841b9080732ebbd7cba77
JoshuaQSH <37936386+JoshuaQSH@users.noreply.github.com>
2019-10-30 21:05:05 +0000

    Update thread_test.cpp

--- thread_test.cpp
+++ thread_test.cpp
@@ -28,10 +28,11 @@ int main()
     thread t(&X::do_sth,&my_x);
     std::thread t1(std::bind(func, 1, 2));
     std::thread t2([](int a, int b){cout << "a = " << a << ", b = " << b << endl;}, 1,2);
+    std::thread t3(func,1,2);
     
-    t.join();
     t1.join();
     t2.join();
+    t3.join();
 
     return 0;
 }
