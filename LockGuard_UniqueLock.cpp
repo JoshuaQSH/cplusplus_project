491368179abe6009cd405c45f98a5a63c9ed1909
JoshuaQSH <37936386+JoshuaQSH@users.noreply.github.com>
2019-10-30 20:53:57 +0000

    Update and rename LockGuard_uniqueLock.cpp to LockGuard_UniqueLock.cpp

--- LockGuard_UniqueLock.cpp
+++ LockGuard_UniqueLock.cpp
@@ -13,18 +13,28 @@ std::unique_lock
 release of mutex if not required on particular code
 pathways
 
+• share_ptr
+Accept a requested memory address, 
+construct a smart pointer object to be stored on the stack, 
+and when the program exits the scope of the stack, 
+the stored memory inside the smart pointer is freed (unless the smart pointer is saved) 
+
 */
 
 #include <list>
 #include <mutex>
 #include <algorithm>
+#include <memory> // For share_ptr
 
 std::list<int> some_list;    // A global variable that is protected by a global mutex
 std::mutex some_mutex;       // Create that can protect the variable above
 
+std::shared_ptr<some_resource> resource_ptr; //share_ptr is a Smart Pointer
+std::mutex resource_mutex;
+
 void add_to_list(int new_value)
 {
-  std::lock_guard<std::mutex> guard(some_mutex);    // 3
+  std::lock_guard<std::mutex> guard(some_mutex); // guard is the name of the std::lock_guard<std::mutex> 'some_mutex' is the variable that needs to be protectd
   some_list.push_back(new_value);
 }
 
@@ -33,3 +43,15 @@ bool list_contains(int value_to_find)
   std::lock_guard<std::mutex> guard(some_mutex);    // 4
   return std::find(some_list.begin(),some_list.end(),value_to_find) != some_list.end();
 }
+
+void foo()
+{
+  std::unique_lock<std::mutex> lk(resource_mutex);  // All the thread should be initialize here
+  if(!resource_ptr)
+  {
+    resource_ptr.reset(new some_resource);  // Only when initializing, the protection is required 
+  }
+  lk.unlock();  // Unlock the 'resource_mutex'
+  resource_ptr->do_something();
+}
+
