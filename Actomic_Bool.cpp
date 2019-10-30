2095cd62fbafec4c606ffa79359db7de04f7197e
JoshuaQSH <37936386+JoshuaQSH@users.noreply.github.com>
2019-10-30 23:01:39 +0000

    Update Actomic_Bool.cpp

--- Actomic_Bool.cpp
+++ Actomic_Bool.cpp
@@ -1,3 +1,20 @@
+/*
+Objects of std::atomic_flag must be initialized to clear
+• std::atomic_flag f = ATOMIC_FLAG_INIT
+• The only available operations are
+• test_and_set()
+• clear()
+
+Objects of std::atomic<bool>
+A more full-featured boolean flag than std::atomic_flag
+std:atomic<bool> b (true);
+• Operations:
+• load() => bool x = b.load();
+• store() => b.store(false);
+• exchange() => x = b.exchange(true);
+
+*/
+
 #include <vector>
 #include <atomic>
 #include <iostream>
