8e1d6a23c8a2dd7fc05445d9135c93fba5ec7b6a
JoshuaQSH <37936386+JoshuaQSH@users.noreply.github.com>
2019-10-30 20:32:25 +0000

    Update DeadLockAndMutex.cpp

--- DeadLockAndMutex.cpp
+++ DeadLockAndMutex.cpp
@@ -3,6 +3,17 @@ Deadlock usually happens when at least two threads have to lock two or more mute
 Use mutex to solve deadlock issue
 Use lock_guard() instead of std::lock()
 
+G #1: Avoid nested locks
+• If you really must seize multiple locks, use
+std::lock()
+• G #2: Avoid calling user-supplied code while holding a lock
+• That code could also acquire locks – hard to track.
+• G #3: Acquire locks in a fixed order
+• Need a way that is consistent between threads
+• May need, e.g., to define a “canonical order of traversal”
+for data structures
+• G #4: Use a lock hierarchy
+
 */
 
 #include <exception>
